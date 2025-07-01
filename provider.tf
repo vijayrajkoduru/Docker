6393c8bc9e69318f93e092c0070e1eb998bd68b9
Vijaykumar-Koduru <koduru.vijaykumar.7@gmail.com>
2025-07-02 01:28:36 +0530

    created

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+  
+  backend "s3" {
+    bucket         = "vijaykumar-dev"
+    key            = "docker-dock"
+    region         = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
